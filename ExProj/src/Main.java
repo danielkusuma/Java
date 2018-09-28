3cb62d58ec6f10f3cdd8b78603cd94f294ebcd8a
Daniel Kusuma <d@nielengineer.com>
2018-09-28 13:42:19 +1000

    commit 13:42

---
+++ ExProj/src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        // Primitive Data Types
+        // Int has a width of 32
+        int minInt = -2_147_483_648;
+        int maxInt = 2_147_483_647;
+        // Byte is more efficient (width = 8)
+        byte minValue = -128;
+        byte maxByte = 127;
+        // Short cost twice of byte's space (width = 16)
+        short minShort = -32768;
+        short maxShort = 31767;
+        // long
+        long maxLong = ;
+        long minLong =
+
+
+
+
+
+        /*
+        // Variables
+        int myFirstNumber = 10 + 200 * 2 + (20 + 5);
+        int mySecondNumber = 5;
+        System.out.println(myFirstNumber);
+        int myTotal = myFirstNumber + mySecondNumber;
+        int lastNumber = myTotal - myFirstNumber;
+        System.out.println(lastNumber);
+        */
+        /*
+        // My First App
+        System.out.println("Hello World");
+        */
+    }
+}
