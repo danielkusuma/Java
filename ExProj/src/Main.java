0fee6836a578670c71487483447c225bb50ed38d
Daniel Kusuma <d@nielengineer.com>
2018-10-08 06:53:10 +1100

    commit

--- ExProj/src/Main.java
+++ ExProj/src/Main.java
@@ -1,23 +1,117 @@
+import java.util.logging.SocketHandler;
+
 public class Main {
     public static void main(String[] args) {
+        boolean gameOver = true;
+        /*
+        // Code Blocks and if then else control statement
+        int score = 500;
+        int levelCompleted = 5;
+        int bonus = 100;
+        if (score == 5000) {
+            System.out.println("Your score is 5000");
+        } else if (score < 1000) {
+            System.out.println("Your score is less than 1000");
+        } else {
+            System.out.println("Got here");
+        }
+        if (gameOver == true){
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+        // cannot read this code block
+        int savedFinalScore = finalScore;
+        */
+        /*
+        // Operators
+        int result = 10 / 2;
+        System.out.println(result);
+        result = result % 2;
+        System.out.println(result);
+        result--;
+        System.out.println(result);
+        result++;
+        System.out.println(result);
+        result += 2;
+        System.out.println(result);
+        result *= 10;
+        System.out.println(result);
+        result /= 10;
+        System.out.println(result);
+        boolean isHuman = false;
+        if (isHuman = false) {
+            System.out.println("It's not human");
+        } else {
+            System.out.println("It's human");
+        }
+        int topScore = 105;
+        if (topScore < 100) {
+            System.out.println("Score = less than 100");
+        } else {
+            System.out.println("Score is 100 or more");
+        }
+        int score = 50;
+        if ((score < topScore) &&(topScore < 200)){
+            System.out.println("You score less than topScore");
+        } else if ((score < topScore) || (topScore < 100)){
+            System.out.println("You score more than topScore");
+        } else ((score < topScore) && (topScore > 100)) {
+            System.out.println("This will produce a text");
+        }
+        boolean isCoupe = true;
+        boolean isCar = isCoupe ? false : true;
+        if (isCar) {
+            System.out.println("Car is coupe");
+        }
+        */
+        /*
+        // String and Finishing Prim. Data Type
+        // Primitive type: byte, short, int, long, float, double, char, boolean
+        // String = sequence of character
+        String myString = "This is my string";
+        // This will be treated as a string
+        String myNumberString = "200";
+        */
+        /*
+        // Char and Boolean
+        // Char can only have 1 character
+        char myChar = 'D';
+        char myUnicode = '\u00A9';
+        System.out.println(myUnicode);
+        // Boolean
+        boolean myBoolean = false;
+        boolean isMale = true;
+        */
+        /*
+        // Float and Double
+        // float is single precision
+        int myInt = 5 / 3;
+        float myFloat = 5f / 3f;
+        double myDouble = 5d / 3d;
+        System.out.println(myInt);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+        */
+        /*
         // Primitive Data Types
-        // Int has a width of 32
+        // Int has a width of 32 (most used)
         int minInt = -2_147_483_648;
         int maxInt = 2_147_483_647;
         // Byte is more efficient (width = 8)
-        byte minValue = -128;
+        byte minByte = -128;
         byte maxByte = 127;
+        // Has to be byte
+        byte myNewByte = (byte)(minByte/2);
+        System.out.println(myNewByte);
         // Short cost twice of byte's space (width = 16)
         short minShort = -32768;
         short maxShort = 31767;
-        // long
-        long maxLong = ;
-        long minLong =
-
-
-
-
-
+        // long has a width of 64
+        long maxLong = 9_223_372_036_854_775_807L;
+        // Create a variable of type long, and make it equal to 500 + 10 times the sum of the byte, plus the short plus the int
+        long longTotal = 500L + 10L * (maxByte + maxShort + maxInt);
+        System.out.println(longTotal);
+        */
         /*
         // Variables
         int myFirstNumber = 10 + 200 * 2 + (20 + 5);
@@ -33,3 +127,4 @@ public class Main {
         */
     }
 }
+
