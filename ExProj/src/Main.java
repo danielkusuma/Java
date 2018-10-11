461c5bed9996ba344891de6d45882a0f0eda43e8
Daniel Kusuma <d@nielengineer.com>
2018-10-11 18:46:06 +1100

    just more practice

--- ExProj/src/Main.java
+++ ExProj/src/Main.java
@@ -1,10 +1,82 @@
+import sun.awt.geom.AreaOp;
+
+import javax.naming.ldap.UnsolicitedNotification;
 import java.util.logging.SocketHandler;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        /*// Methods in Java (see bottom file)
+        System.out.println("10k at 10% interest: " + calculateInterest(1000, 10));
+
+    }
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate / 100));
+    }
+        /*
+        // if else
+        int value = 1;
+        if (value == 1) {
+            System.out.println("Value was 1");
+        } else if ( value == 2 ) {
+            System.out.println("Value was 2");
+        } else {
+            System.out.println("Was not 1 or 2");
+        }
+        // switch
+        int switchValue = 1;
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            case 3: case 4: case 5:
+                System.out.println("Value  was 3/4/5");
+                System.out.println("Actually It was a " + switchValue);
+                break;
+            default:
+                System.out.println("Was not 1 or 2");
+                break;
+        }
+        // Challenge
+        // Create a new switch statement using char instead of int
+        // Create a new char variable
+        // Create a switch statement testing for A, B, C, D, E
+        // Display a message if any of these are found and then break
+        // Add a default which displays a message saying not found
+        char switchChar = 'A';
+        switch (switchChar) {
+            case 'A':
+                System.out.println("A");
+                break;
+            case 'B':
+                System.out.println("B");
+                break;
+            case 'C':
+                System.out.println("C");
+                break;
+            default:
+                System.out.println("Not found");
+                break;
+        }
+        // string switch
+        String choice = "january";
+        switch(choice.toLowerCase()) {
+            case "January":
+                System.out.println("Jan");
+                break;
+            case "June":
+                System.out.println("Jun");
+                break;
+            case "Feb":
+                System.out.println("Feb");
+                break;
+        }
+        */
+        /*
+        // Methods in Java (see bottom file)
         // calculateScore(true, 800, 5, 100);
         // calculateScore(false, 400, 5, 30);
         int playerScore = calculateHighScorePosition(300);
@@ -12,7 +84,7 @@ public class Main {
         /*
         // Code Blocks and if then else control statement
         int score = 500;
-        int levelCompleted = 8;
+        int levelCompleted = 8;/..l;
         int bonus = 200;
         if (score == 5000) {
             System.out.println("Your score is 5000");
