b75cea7cce7dc541c5eecfb94e4e713a5664397e
Daniel Kusuma <d@nielengineer.com>
2018-10-13 21:51:19 +1100

    a

--- ExProj/src/Main.java
+++ ExProj/src/Main.java
@@ -1,6 +1,7 @@
 import sun.awt.geom.AreaOp;
 
 import javax.naming.ldap.UnsolicitedNotification;
+import java.sql.SQLOutput;
 import java.util.logging.SocketHandler;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,10 +9,48 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         System.out.println("10k at 10% interest: " + calculateInterest(1000, 10));
+//        for(int i = 0; i < 5; i++){
+//            System.out.println("Loop: " + i + " hello");
+//        }
+        // Using the statement, call the calculateInterest method with
+        // the amount of 10000 with an interestRate of 2, 3, 4, 5, 6, 7, 8
+        // and print the results to the console window.
+//        for(int i = 2; i < 9;i++) {
+//            System.out.println("10k at " + i + " interest: " + String.format("%.2f", calculateInterest(10000, i)));
+//        }
 
+        // How would you modify the for loop above to do the same thing as
+        // shown but to start from 8% and work back to 2%
+//        for(int j = 8; j > 1;j--) {
+//            System.out.println("10k at " + j + " interest: " + String.format("%.2f", calculateInterest(10000, j)));
+//        }
+
+        // Create a for statement using any range of numbers
+        // Determine if the number is a prime number using the isPrime method
+        // if it is a prime number, print it out and increment a count of the
+        // number of prime numbers found
+        // if that count is 3 exit for the loop
+        // hint: use the break; statement to exit
+        int n = 1;
+        isPrime(1);
+        if (isPrime = true) {
+            System.out.println(n);
+        }
     }
     public static double calculateInterest(double amount, double interestRate) {
         return (amount * (interestRate / 100));
+    }
+
+    public static boolean isPrime(int n) {
+        if(n == 1) {
+            return false;
+        }
+        for(int i = 2; i <= n/2; i++) {
+            if(n % 1 == 0) {
+                return false;
+            }
+        }
+        return true;
     }
         /*
         // if else
