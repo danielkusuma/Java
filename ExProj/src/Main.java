54f268ccb3f62e0feb3ac5579700f8197c354fa4
Daniel Kusuma <d@nielengineer.com>
2018-10-17 09:15:42 +1100

    no message

--- ExProj/src/Main.java
+++ ExProj/src/Main.java
@@ -8,13 +8,6 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-    // Debugger
-        StringUtilities utils = new StringUtilities();
-        StringBuilder sb = new StringBuilder();
-        while (sb.length() < 10) {
-            utils.addChar(sb, 'a');
-        }
-        System.out.println(sb);
     }
 }
 
